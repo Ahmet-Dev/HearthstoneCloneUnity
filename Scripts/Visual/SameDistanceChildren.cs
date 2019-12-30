8b6f60f5c5a8d0bd465bb7bc907cb2eb0240aecd
Ahmet <53380248+Ahmet-Dev@users.noreply.github.com>
2019-12-30 22:33:52 +0300

    Update SameDistanceChildren.cs

--- Scripts/Visual/SameDistanceChildren.cs
+++ Scripts/Visual/SameDistanceChildren.cs
@@ -1,8 +1,5 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-// place first and last elements in children array manually
-// others will be placed automatically with equal distances between first and last elements
 public class SameDistanceChildren : MonoBehaviour {
 
     public Transform[] Children;
